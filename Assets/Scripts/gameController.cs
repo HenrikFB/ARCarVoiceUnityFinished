4c1792e45b4077d5c0d0eab826b54a8d6b779892
HenrikFB <henrik_1993@hotmail.com>
2018-10-17 13:24:33 +0200

    almost finished.
    Jeg mangler project 1(end) ground planes og lektion 59 og 60.
    
    Jeg skal ikke bruge deployonce.route, da jeg er i 2018 versionen. Skal nok nok bare lave den public static... route = ... og så insætte route hvor i den tidligere version bruger deployonce... .route

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameController : MonoBehaviour {
+
+    public static string currentSelectedCar = "myPorsche";//"myLamboConvert";//"myPorsche";//"Tocus";//"myDownloadedLambo";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void showNewCar(string nextCar)
+    {
+        GameObject.Find(colourSwitcher.instance.getCurrentTracked().name + "/activeItems/" + gameController.currentSelectedCar).SetActive(false);
+        currentSelectedCar = nextCar;
+        GameObject.Find(colourSwitcher.instance.getCurrentTracked().name + "/activeItems/" + gameController.currentSelectedCar).SetActive(true);
+    }
+
+
+    public void quit() {
+        Application.Quit();
+    }
+
+    public void changeLevel(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+	
+}
---
+++ Assets/infoPanelAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class infoPanelAnim : MonoBehaviour {
+
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    public void playSlide() {
+        anim.Play("infoAnim");
+    }
+
+    public void closeSlide() {
+        anim.Play("infoAnimReverse");
+    }
+}
